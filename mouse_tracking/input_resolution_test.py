86a8af8377dba35a4258097e76f083ad412df04a
PattonYin <pyin@wesleyan.edu>
2024-03-22 04:56:00 -0400

    analyzed the trajectory of flicks with mouse

--- mouse_tracking/input_resolution_test.py
+++ mouse_tracking/input_resolution_test.py
@@ -18,27 +18,31 @@ def pyautogui_test():
 
 def flick_test():
     mouse = Controller()
-    x_list = []
-    y_list = []
-    time_list = []
-    with open("mouse_tracking/sample/flick_01.txt", "r") as flick_data:
-        # Sample line mouse,701,580,1711094738.2393444
-        # read all the x, y, and time data in one go
-        data = flick_data.readlines()
-        for line in data:
-            line = line.strip().split(",")
-            x_list.append(int(line[1]))
-            y_list.append(int(line[2]))
-            time_list.append(float(line[3]))
-    
-    # Target time 0.1867809295654297
-    start = time.time()
-    scale = 3
-    for i in range(1, len(x_list)):
-        mouse.position = (x_list[i], y_list[i])
-        time.sleep((time_list[i]-time_list[i-1])*0.8)        
-    end = time.time()
-    print(f"Elapsed time: {end-start}")
+
+    for i in range(3):
+        x_list = []
+        y_list = []
+        time_list = []
+        with open(f"mouse_tracking/sample/flicks/{i+1}.txt", "r") as flick_data:
+            # Sample line mouse,701,580,1711094738.2393444
+            # read all the x, y, and time data in one go
+            data = flick_data.readlines()
+            for line in data:
+                line = line.strip().split(",")
+                if line[0] == "mouse":                    
+                    x_list.append(int(line[1]))
+                    y_list.append(int(line[2]))
+                    time_list.append(float(line[3]))
+        
+        # Target time 0.1867809295654297
+        start = time.time()
+        scale = 0.835
+        for i in range(1, len(x_list)):
+            mouse.position = (x_list[i], y_list[i])
+            time.sleep((time_list[i]-time_list[i-1])*scale)        
+        end = time.time()
+        print(f"Actual time: {end-start}")
+        print(f"Expected time: {time_list[-1]-time_list[0]}")
     
 if __name__ == "__main__":
     # pynput_test()

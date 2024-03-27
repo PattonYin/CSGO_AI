81e781f7cc7aaef7194ad5913ffe8f7656cc692f
PattonYin <pyin@wesleyan.edu>
2024-03-27 13:50:58 -0400

    data collect code done #12

--- demo_analysis/data_processor.py
+++ demo_analysis/data_processor.py
@@ -1,5 +1,6 @@
 from demoparser2 import DemoParser
 import pandas as pd
+import matplotlib.pyplot as plt
 
 class Processor:
     def __init__(self, path="demo_analysis/demo/furia-vs-lynn-vision-nuke.dem"):
@@ -9,9 +10,27 @@ class Processor:
         df_player = self.parser.parse_ticks(wanted_props=["X", "Y", "Z", "pitch", "yaw"])
         return df_player
     
-if __name__ == "__main__":
-    demo_path = "demo_analysis/demo/furia-vs-lynn-vision-nuke.dem"
-    processor = Processor(demo_path)
-    df_player = processor.get_player_data()
-    print(len(df_player)/(640*60))
+    def plot_attempt(self, player_name="z4KR"):
+        df_player = self.get_player_data()
+        df_player = df_player[df_player["name"] == player_name]
+        df_player = df_player[:]
+        print(df_player.head(20))
+        # plot out in dots, with X as x-axis and Y as y-axis
+        plt.scatter(df_player["X"], df_player["Y"], s=1)
+        plt.show()
+        
     
+if __name__ == "__main__":
+    match_01 = {
+        "demo_path": "demo_analysis/demo/furia-vs-lynn-vision-nuke.dem",
+        "player_name": "z4KR"
+    }
+    match_02 = {
+        "demo_path": "demo_analysis/demo/match730_003673760416913162325_1520768583_129.dem",
+        "player_name": "Aerith"
+    }
+
+    processor = Processor(match_02["demo_path"])
+    # df_player = processor.get_player_data()
+    # print(len(df_player)/(640*60))
+    processor.plot_attempt(match_02["player_name"])
